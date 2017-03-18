997083f12aee4454a3f72f638f7059b9505e13fb
RyoTanimura <zeromegalpha@gmail.com>
2017-03-18 01:38:32 +0000

    ランキング追加

---
+++ app/controllers/ranking_controller.rb
@@ -0,0 +1,13 @@
+class RankingController < ApplicationController
+  
+  def want
+    item_ids = Want.group(:item_id).order('count_all desc').limit(10).count.keys
+    @items = Item.find(item_ids).sort_by{|item| item_ids.index(item.id)}
+  end
+  
+  def have
+    item_ids = Have.group(:item_id).order('count_all desc').limit(10).count.keys
+    @items = Item.find(item_ids).sort_by{|item| item_ids.index(item.id)}
+  end
+  
+end
